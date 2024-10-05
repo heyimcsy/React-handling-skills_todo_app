b73b9748cdf127c0839abe58bc4d8d15611f74c5
sychoi <cinnamon1995@naver.com>
2024-10-05 15:06:12 +0900

    Create todo list template

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import TodoTemplate from './components/TodoTemplate';
 import TodoInsert from './components/TodoInsert';
+import TodoList from './components/TodoList';
 
 const App = () => {
-  return <TodoTemplate><TodoInsert/></TodoTemplate>
+  return <TodoTemplate><TodoInsert/><TodoList/></TodoTemplate>
 }
 export default App;

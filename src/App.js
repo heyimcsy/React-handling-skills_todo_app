5cfb481754ba14d52794b6cbef4c45ce7e15ffb4
sychoi <cinnamon1995@naver.com>
2024-10-05 16:09:56 +0900

    Create toggle function

--- src/App.js
+++ src/App.js
@@ -40,6 +40,15 @@ const App = () => {
       setTodos(todos.filter(todo => todo.id !== id))
     },[todos]
   )
-  return <TodoTemplate><TodoInsert onInsert={onInsert}/><TodoList todos={todos}  onRemove={onRemove}/></TodoTemplate>
+
+  const onToggle = useCallback(
+    id => {
+      setTodos(
+        todos.map(todo =>
+        todo.id === id ? {...todo, checked : !todo.checked} : todo)
+      )
+    },[todos]
+  )
+  return <TodoTemplate><TodoInsert onInsert={onInsert}/><TodoList todos={todos}  onRemove={onRemove} onToggle={onToggle}/></TodoTemplate>
 }
 export default App;
